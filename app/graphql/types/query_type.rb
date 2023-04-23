20f4873a729223e68f2ccf8dd0b33daa5f0a7cb2
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-04-23 08:40:00 +0300

    Added posts and users types

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -8,10 +8,12 @@ module Types
     # They will be entry points for queries on your schema.
 
     # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :users, [UserType], null: false,
+      description: "Returns a list of users in the database"
+
+    def users
+      User.all
     end
+    
   end
 end
---
+++ app/graphql/types/user_type.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Types
+  class UserType < Types::BaseObject
+    field :id, ID, null: false
+    field :first_name, String
+    field :last_name, String
+    field :address, String
+    field :postcode, String
+    field :city, String
+    field :posts , [Types::PostType], null: true
+    field :country, String
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end
